2fdbcc0d728620a9996d3c112b96b0b523567964
AlexBim <centuriin@yandex.ru>
2021-03-13 20:36:41 +0300

    добавлены проекты

---
+++ ConsoleModule/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleModule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HashBL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace HashBL
+{
+    public class Class1
+    {
+    }
+}
